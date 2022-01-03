6a66f2d864b741fbaa13ff82e487d7bf3503c235
Armin Hupka <arminhupka@me.com>
2022-01-03 12:53:23 +0100

    - updated footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import FooterLinkWidget from './FooterLinkWidget/FooterLinkWidget';
 
 const Footer = () => (
   <footer className="text-white">
     <div className="footer-main bg-gray-800">
-      <div className="container py-20 md:flex md:space-x-20">
-        <FooterLinkWidget title="Information" />
-        <FooterLinkWidget title="Inventory" />
-        <FooterLinkWidget title="Useful Links" />
-      </div>
       <div className="footer-copy bg-gray-900">
         <div className="container py-8">
           <div>
             <span className="text-gray-400 font-semibold">
-              Developed by <span className="text-red-500">@arminhupka</span>
+              Developed by{' '}
+              <span className="text-red-500">
+                <a href="https://github.com/arminhupka">@arminhupka</a>
+              </span>
             </span>
           </div>
         </div>
